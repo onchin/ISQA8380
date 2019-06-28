fcdd007798c8fe55995591efec9899a8ab61206c
onchin <42681382+onchin@users.noreply.github.com>
2019-06-28 00:03:51 -0500

    Add files via upload

---
+++ components/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="about">
+        <h1>This is {{ their_name }}'s To Do list developed in Vue.Js to learn Vue.Js</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur......</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    } 
+  }
+}
+</script>
+
+
+
+
+
